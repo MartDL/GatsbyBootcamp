060de35826573fb84be78817d1fe85ad4de2d8a6
Martin Lord <Mart1981@marts-air.home>
2019-08-14 07:06:44 +0100

    stage 13

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ContactPage = () => {
+    return (
+        <Layout>
+        <h1>Contact</h1>
+        <p>The best way to reach me is through my <a href="https://twitter.com/Chopsta1">on twitter</a></p>
+    </Layout>
+    )
+}
+
+export default ContactPage;
---
+++ src/posts/gatsby.md
@@ -0,0 +1,12 @@
+---
+title: "The Great Gatsby Bootcamp"
+date: "2019-12-08"
+---
+
+I just launched a new bootcamp !!
+
+##
+
+1. Gatsby
+2. GraphQL
+3. React
